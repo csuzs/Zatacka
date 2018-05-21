eb6c39abff393695b58405b9db49f60650e8f849
vilthuril <edina.kocsis13@gmail.com>
2018-05-21 12:49:18 +0200

    first commit

---
+++ network/Message.hpp
@@ -0,0 +1,18 @@
+#ifndef CHAT_MESSAGE_HPP
+#define CHAT_MESSAGE_HPP
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+class Message{
+public:
+static const int MAX_HEADER_LENGTH;
+static const int MAX_BODY_LENGTH;
+
+private:
+char data_[MAX_HEADER_LENGTH + MAX_BODY_LENGTH];
+size_t body_length;
+
+};
+
+#endif
